52414c01127b76328e5b3a4c23264736fb5b9485
AsrBassam <45674276+AsrBassam@users.noreply.github.com>
2018-12-21 09:40:50 -0800

    Add files via upload

---
+++ dndn.js
@@ -0,0 +1,30 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+:kissing::heart::eyes: اهلا بك في سيرفرنا المتواضع 
+ᏔᎬᏞᏨᏫᎷᎬ  ᎿᏫ ᏚᎬᎡᏉᎬᎡ ᎡᎬᎠ Ꭿ ᎡᎷᎽ
+
+[ ${member}  ]
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+:kissing::heart::eyes: اهلا بك في سيرفرنا المتواضع 
+ᏔᎬᏞᏨᏫᎷᎬ  ᎿᏫ ᏚᎬᎡᏉᎬᎡ ᎡᎬᎠ Ꭿ ᎡᎷᎽ
+
+**`) 
+}).catch(console.error)
+})
+client.login("NTI1NzI2MDc3NDg2NzU5OTM2.Dv60bg.Be5nitrH0cysHpk7UjM6GN4N2xs"
+);
